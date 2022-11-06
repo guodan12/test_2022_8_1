7380be583cfac5f3945cc44db6ff2037ffb3ef15
guodan12 <1628322965@qq.com>
2022-11-06 20:44:21 +0800

    2.cpp

---
+++ 2.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Solution {
+public:
+	int age;
+	
+public:
+	Solution(){};
+	~Solution(){};
+	Solution(const Solution&){
+		cout << 111 << endl;
+	}
+	
+public:
+	void test() {
+		cout << "yes" << endl;
+	}
+};
+
+int main()
+{
+	Solution s;
+	s.test();
+	return 0;
+}
